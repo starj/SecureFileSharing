461a5e625491f5b4dc8d290cbe50434dbd443794
starj <asin489363@gmail.com>
2024-07-31 15:24:37 +0200

    optimization improvement

--- main.rs
+++ main.rs
@@ -1,9 +1,30 @@
+[dependencies]
+actix-web = "4.0"
+dotenv = "0.15.0"
+lazy_static = "1.4.0"
+tokio = { version = "1", features = ["full"] }
+```
+
+```rust
 use actix_web::{web, App, HttpResponse, HttpServer};
 use dotenv::dotenv;
 use std::env;
+use std::sync::Mutex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref INDEX_CACHE: Mutex<Option<String>> = Mutex::new(None);
+}
 
 async fn index() -> HttpResponse {
-    HttpResponse::Ok().body("Hello from Actix-web!")
+    let mut cache = INDEX_CACHE.lock().unwrap();
+    if let Some(cached_value) = &*cache {
+        HttpResponse::Ok().body(cached_value.clone())
+    } else {
+        let response_body = "Hello from Actix-web!".to_string();
+        *cache = Some(response_body.clone());
+        HttpResponse::Ok().body(response_body)
+    }
 }
 
 #[actix_web::main]
